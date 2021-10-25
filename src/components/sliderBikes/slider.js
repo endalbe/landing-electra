eae466cea9279c4c86d63f24dddc753bf94a2520
Evgeny Agapov <evgenyagap@gmail.com>
2021-10-25 17:48:50 +0300

    Bike slider added and done

--- src/components/sliderBikes/slider.js
+++ src/components/sliderBikes/slider.js
@@ -3,11 +3,26 @@ import './slider.scss';
 import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
-let sections = gsap.utils.toArray('.slide-bikes');
+let sections = gsap.utils.toArray('.slide-bike');
 gsap.registerPlugin(ScrollTrigger);
 
 let waitingLoad = setInterval(() => {
 	if (!document.querySelector('#app').classList.contains('hide')) {
+		sections.forEach((slide) => {
+			gsap.to(slide, {
+				scrollTrigger: {
+					trigger: slide,
+					scrub: true,
+					pin: true,
+					start: 'center center',
+					end: 'bottom -100%',
+					toggleClass: 'active',
+					ease: 'power2',
+					snap: 1
+				}
+			});
+		});
+
 		clearInterval(waitingLoad);
 	}
 }, 1000);
