cfa55e18e476026d686d56c2d864c1a7f25ee510
Evgeny Agapov <35006451+endalbe@users.noreply.github.com>
2021-10-30 16:43:12 +0300

    Benefits section is done

--- src/Invest/hooker.js
+++ src/Invest/hooker.js
@@ -2,3 +2,4 @@ import './components/header/header';
 import './components/secondSection/index';
 import './components/about/about';
 import './components/delimiter/delimiter';
+import './components/benefits/benefits';
