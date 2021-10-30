326cbb8c307ba40a7a1224e50719900df2bbe377
Evgeny Agapov <35006451+endalbe@users.noreply.github.com>
2021-10-30 19:17:51 +0300

    Expand section is done

--- src/Invest/hooker.js
+++ src/Invest/hooker.js
@@ -4,3 +4,4 @@ import './components/about/about';
 import './components/delimiter/delimiter';
 import './components/benefits/benefits';
 import './components/table/table';
+import './components/expand/expand';
